db1b37eddf9f95573ba28e5c4f894a867d4c4346
madhavdua25 <128146906+madhavdua25@users.noreply.github.com>
2024-10-07 08:38:47 -0400

    updated titles

--- src/main/java/bca/MainFrame.java
+++ src/main/java/bca/MainFrame.java
@@ -24,29 +24,29 @@ public class MainFrame extends Application {
         loadMenu(primaryStage);
 }
     public static void loadMenu (Stage stage) {        
-        Button btHBox = new Button("HBox Example");
-        Button btVBox = new Button("VBox Example");
-        Button btGrid = new Button("Grid Example");
-        Button btBorderPane = new Button("BorderPane Example");
-        Button btMixAndMatch = new Button("Mix and Match Demo");
+        Button btHBox = new Button("Scored Quiz");
+        Button btVBox = new Button("Unscored Practice");
+        Button btGrid = new Button("Score Report");
+       //Button btBorderPane = new Button("BorderPane Example");
+       //Button btMixAndMatch = new Button("Mix and Match Demo");
 
         btHBox.setOnAction(e-> HBoxExamplePage.showHBoxExample(stage));
         btVBox.setOnAction(e-> VBoxExamplePage.showVBoxExample(stage));
         btGrid.setOnAction(e-> GridExamplePage.showGridExample(stage));
-        btBorderPane.setOnAction(e-> BorderPaneExamplePage.showBorderPaneExample(stage));
-        btMixAndMatch.setOnAction(e-> MixMatchDemo.showMixMatchDemo(stage));
+        //btBorderPane.setOnAction(e-> BorderPaneExamplePage.showBorderPaneExample(stage));
+        //btMixAndMatch.setOnAction(e-> MixMatchDemo.showMixMatchDemo(stage));
 
         GridPane gp = new GridPane();
         gp.setAlignment(Pos.TOP_CENTER);
         gp.add(btHBox, 0, 1);
         gp.add(btVBox, 1, 1);
         gp.add(btGrid, 2, 1);
-        gp.add(btBorderPane, 3, 1);
-        gp.add(btMixAndMatch, 4, 1);
+        //gp.add(btBorderPane, 3, 1);
+        //gp.add(btMixAndMatch, 4, 1);
         gp.setHgap(10);
         gp.setVgap(10);
 
-        Label pageTitle = new Label("Main menu");
+        Label pageTitle = new Label("Learn Russian!");
         pageTitle.setFont(MainFrame.PAGE_HEADING_FONT);
         pageTitle.setPadding(new Insets(0,0,30,0));
         
