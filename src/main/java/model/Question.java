d31aae5af8a41cd85db54fa5ed9195b238be6998
madhavdua25 <128146906+madhavdua25@users.noreply.github.com>
2024-10-21 08:49:12 -0400

    question file

--- src/main/java/model/Question.java
+++ src/main/java/model/Question.java
@@ -1,5 +1,60 @@
 package model;
 
 public class Question {
+    private int question_id;
+    private String text;
+    private String incorrect1;
+    private String incorrect2;
+    private String incorrect3;
+
+    public Question(int question_id, String text, String incorrect1, String incorrect2, String incorrect3) {
+        this.question_id = question_id;
+        this.text = text;
+        this.incorrect1 = incorrect1;
+        this.incorrect2 = incorrect2;
+        this.incorrect3 = incorrect3;
+    }
+
+    public int getQuestion_id() {
+        return question_id;
+    }
+
+    public void setQuestion_id(int question_id) {
+        this.question_id = question_id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getIncorrect1() {
+        return incorrect1;
+    }
+
+    public void setIncorrect1(String incorrect1) {
+        this.incorrect1 = incorrect1;
+    }
+
+    public String getIncorrect2() {
+        return incorrect2;
+    }
+
+    public void setIncorrect2(String incorrect2) {
+        this.incorrect2 = incorrect2;
+    }
+
+    public String getIncorrect3() {
+        return incorrect3;
+    }
+
+    public void setIncorrect3(String incorrect3) {
+        this.incorrect3 = incorrect3;
+    }
+
+    
     
 }
